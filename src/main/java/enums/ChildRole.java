5863161dc6d090d400788128c8440c8b70c2d90b
Ramon <ramonrodsouza@gmail.com>
2025-06-02 16:44:10 -0300

    feature-002 - feat: criando enums, AgeGroup, ChildRole, CivilStatus, Role

---
+++ src/main/java/enums/ChildRole.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum ChildRole {
+    EMPTY,
+    MEMBER,
+    VOLUNTEER;
+}
---
+++ src/main/java/enums/Role.java
@@ -0,0 +1,9 @@
+package enums;
+
+public enum Role {
+    STUDENT,
+    MEMBER,
+    VOLUNTEER,
+    LEADER,
+    PASTOR
+}
