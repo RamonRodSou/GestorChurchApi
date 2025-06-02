660f1c25335ada6b97de647241e4d0d07061aad7
Ramon <ramonrodsouza@gmail.com>
2025-06-02 16:44:47 -0300

    feature-004 - feat: criando entity Child

---
+++ src/main/java/user/child/ChildSummary.java
@@ -0,0 +1,15 @@
+package user.child;
+
+public class ChildSummary {
+    private Long id;
+    private String name;
+    private String phone;
+
+    public ChildSummary() { }
+
+    public ChildSummary(Long id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+}
