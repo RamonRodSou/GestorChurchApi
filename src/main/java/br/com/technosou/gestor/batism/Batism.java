2d5091e4da05087ac757f925398c6e1202032e9a
Ramon <ramonrodsouza@gmail.com>
2025-06-23 21:51:10 -0300

    feature-012 - feat: criando classe Batismo e seus get e sets

---
+++ src/main/java/br/com/technosou/gestor/batism/Batism.java
@@ -0,0 +1,69 @@
+package br.com.technosou.gestor.batism;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Batism {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String churchName;
+    private boolean ourChurch = true;
+    public String leaderName;
+    public Date baptismDate;
+
+    public Batism() { }
+
+    public Batism(String churchName, boolean ourChurch, String leaderName, Date baptismDate) {
+        this.churchName = churchName;
+        this.ourChurch = ourChurch;
+        this.leaderName = leaderName;
+        this.baptismDate = baptismDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getChurchName() {
+        return churchName;
+    }
+
+    public void setChurchName(String churchName) {
+        this.churchName = churchName;
+    }
+
+    public boolean isOurChurch() {
+        return ourChurch;
+    }
+
+    public void setOurChurch(boolean ourChurch) {
+        this.ourChurch = ourChurch;
+    }
+
+    public String getLeaderName() {
+        return leaderName;
+    }
+
+    public void setLeaderName(String leaderName) {
+        this.leaderName = leaderName;
+    }
+
+    public Date getBaptismDate() {
+        return baptismDate;
+    }
+
+    public void setBaptismDate(Date baptismDate) {
+        this.baptismDate = baptismDate;
+    }
+}
