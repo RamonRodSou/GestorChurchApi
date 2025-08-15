ce4e8b22a64f3a04c33dc9f93a3bfbe48b0e82bf
Ramon <ramonrodsouza@gmail.com>
2025-08-15 18:14:27 -0300

    feature-34 - feat: Add atributos em Member

--- src/main/java/br/com/technosou/gestor/member/Member.java
+++ src/main/java/br/com/technosou/gestor/member/Member.java
@@ -1,8 +1,12 @@
 package br.com.technosou.gestor.member;
 
+import br.com.technosou.gestor.group.Group;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Objects;
 
 @MappedSuperclass
 public abstract class Member {
@@ -28,9 +32,23 @@ public abstract class Member {
 
     @Column(length = 13)
     private String phone;
-    private String groupId;
 
-    public Member(Long id, String firstName, String lastName, Date birthdate, String gender, String email, String phone, String groupId) {
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    @JsonManagedReference
+    private Group group;
+
+    @Column(nullable = false)
+    private boolean isImageAuthorized = true;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Member() {}
+
+    public Member(Long id, String firstName, String lastName, Date birthdate, String gender, String email, String phone, Group group, boolean isImageAuthorized, boolean isActive, LocalDateTime createdAt) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -38,10 +56,11 @@ public abstract class Member {
         this.gender = gender;
         this.email = email;
         this.phone = phone;
-        this.groupId = groupId;
+        this.group = group;
+        this.isImageAuthorized = isImageAuthorized;
+        this.isActive = isActive;
+        this.createdAt = createdAt;
     }
-
-    public Member() {}
 
     public Long getId() {
         return id;
@@ -99,11 +118,64 @@ public abstract class Member {
         this.phone = phone;
     }
 
-    public String getGroupId() {
-        return groupId;
+    public Group getGroup() {
+        return group;
     }
 
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public boolean isImageAuthorized() {
+        return isImageAuthorized;
+    }
+
+    public void setImageAuthorized(boolean imageAuthorized) {
+        isImageAuthorized = imageAuthorized;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return isImageAuthorized == member.isImageAuthorized && isActive == member.isActive && Objects.equals(id, member.id) && Objects.equals(firstName, member.firstName) && Objects.equals(lastName, member.lastName) && Objects.equals(birthdate, member.birthdate) && Objects.equals(gender, member.gender) && Objects.equals(email, member.email) && Objects.equals(phone, member.phone) && Objects.equals(group, member.group) && Objects.equals(createdAt, member.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, birthdate, gender, email, phone, group, isImageAuthorized, isActive, createdAt);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthdate=" + birthdate +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", group=" + group +
+                ", isImageAuthorized=" + isImageAuthorized +
+                ", isActive=" + isActive +
+                ", createdAt=" + createdAt +
+                '}';
     }
 }
