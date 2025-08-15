185b52b37611bc4910a835e621a3f2ddee9380aa
Ramon <ramonrodsouza@gmail.com>
2025-08-15 18:11:15 -0300

    feature-029 - refactor: Ajustando update e colocando equal e hashcode em Adult

--- src/main/java/br/com/technosou/gestor/member/adult/Adult.java
+++ src/main/java/br/com/technosou/gestor/member/adult/Adult.java
@@ -1,9 +1,14 @@
 package br.com.technosou.gestor.member.adult;
 
 import br.com.technosou.gestor.batism.Batism;
+import br.com.technosou.gestor.batism.BatismDTO;
 import br.com.technosou.gestor.enums.CivilStatus;
 import br.com.technosou.gestor.enums.Role;
+import br.com.technosou.gestor.group.Group;
+import br.com.technosou.gestor.location.Location;
+import br.com.technosou.gestor.location.LocationDTO;
 import br.com.technosou.gestor.member.Member;
+import br.com.technosou.gestor.member.child.ChildSummaryDTO;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import br.com.technosou.gestor.member.child.Child;
@@ -12,6 +17,7 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "adults")
@@ -22,25 +28,8 @@ public class Adult extends Member implements Serializable {
     @Column(nullable = false, length = 11)
     private String cpf;
 
-    private String groupId;
-
-    @Column(nullable = false, length = 500)
-    private String zipCode;
-
-    @Column(nullable = false, length = 100)
-    private String street;
-
-    @Column(nullable = false, length = 50)
-    private String houseNumber;
-
-    @Column(nullable = false, length = 100)
-    private String city;
-
-    @Column(nullable = false, length = 100)
-    private String state;
-
-    @Column(nullable = false, length = 100)
-    private String neighborhood;
+    @Embedded
+    private Location location;
 
     @Embedded
     private Batism batism;
@@ -60,32 +49,19 @@ public class Adult extends Member implements Serializable {
     @Column(nullable = false)
     private Role role;
 
-    private boolean isActive = true;
-    private boolean isImageAuthorized = true;
-    private LocalDateTime createdAt = LocalDateTime.now();
-
     public Adult() {
         super();
     }
 
-    public Adult(Long id, String firstName, String lastName, Date birthdate, String gender, String email, String phone, String groupId, String cpf, String groupId1, String zipCode, String street, String houseNumber, String city, String state, String neighborhood, Batism batism, CivilStatus civilStatus, Adult spouse, List<Child> children, Role role, boolean isActive, boolean isImageAuthorized, LocalDateTime createdAt) {
-        super(id, firstName, lastName, birthdate, gender, email, phone, groupId);
+    public Adult(Long id, String firstName, String lastName, Date birthdate, String gender, String email, String phone, Group group, boolean isImageAuthorized, boolean isActive, LocalDateTime createdAt, String cpf, Location location, Batism batism, CivilStatus civilStatus, Adult spouse, List<Child> children, Role role) {
+        super(id, firstName, lastName, birthdate, gender, email, phone, group, isImageAuthorized, isActive, createdAt);
         this.cpf = cpf;
-        this.groupId = groupId1;
-        this.zipCode = zipCode;
-        this.street = street;
-        this.houseNumber = houseNumber;
-        this.city = city;
-        this.state = state;
-        this.neighborhood = neighborhood;
+        this.location = location;
         this.batism = batism;
         this.civilStatus = civilStatus;
         this.spouse = spouse;
         this.children = children;
         this.role = role;
-        this.isActive = isActive;
-        this.isImageAuthorized = isImageAuthorized;
-        this.createdAt = createdAt;
     }
 
     public String getCpf() {
@@ -96,62 +72,12 @@ public class Adult extends Member implements Serializable {
         this.cpf = cpf;
     }
 
-    @Override
-    public String getGroupId() {
-        return groupId;
+    public Location getLocation() {
+        return location;
     }
 
-    @Override
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(String houseNumber) {
-        this.houseNumber = houseNumber;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getNeighborhood() {
-        return neighborhood;
-    }
-
-    public void setNeighborhood(String neighborhood) {
-        this.neighborhood = neighborhood;
+    public void setLocation(Location location) {
+        this.location = location;
     }
 
     public Batism getBatism() {
@@ -194,30 +120,6 @@ public class Adult extends Member implements Serializable {
         this.role = role;
     }
 
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public boolean isImageAuthorized() {
-        return isImageAuthorized;
-    }
-
-    public void setImageAuthorized(boolean imageAuthorized) {
-        isImageAuthorized = imageAuthorized;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
     public static Adult AdultUpdate(AdultDTO dto, Adult entity) {
         entity.setFirstName(dto.getFirstName());
         entity.setLastName(dto.getLastName());
@@ -226,22 +128,79 @@ public class Adult extends Member implements Serializable {
         entity.setCpf(dto.getCpf());
         entity.setEmail(dto.getEmail());
         entity.setPhone(dto.getPhone());
-        entity.setGroupId(dto.getGroupId());
-        entity.setZipCode(dto.getZipCode());
-        entity.setStreet(dto.getStreet());
-        entity.setHouseNumber(dto.getHouseNumber());
-        entity.setCity(dto.getCity());
-        entity.setState(dto.getState());
-        entity.setNeighborhood(dto.getNeighborhood());
-//        entity.setBatism(dto.getBatism());
         entity.setCivilStatus(dto.getCivilStatus());
-//        entity.spouse(dto.getSpouseId());
-//        entity.setChildren(dto.getChildrenIds());
         entity.setRole(dto.getRole());
         entity.setImageAuthorized(dto.isImageAuthorized());
         entity.setActive(dto.isActive());
         entity.setCreatedAt(dto.getCreatedAt());
+
+        if (entity.getSpouse() != null) {
+            var spouse = new AdultSummaryDTO(
+                    entity.getSpouse().getId(),
+                    entity.getSpouse().getFirstName(),
+                    entity.getSpouse().getLastName(),
+                    entity.getSpouse().getBirthdate(),
+                    entity.getSpouse().getEmail(),
+                    entity.getSpouse().getPhone()
+            );
+        }
+
+        if (entity.getChildren() != null) {
+           var children = entity.getChildren().stream()
+                    .map(child -> new ChildSummaryDTO(
+                            child.getId(),
+                            child.getFirstName(),
+                            child.getLastName(),
+                            child.getPhone()
+                    ))
+                    .toList();
+        }
+
+        if (entity.getBatism() != null) {
+            var batism = new BatismDTO(
+                    entity.getBatism().getChurchName(),
+                    entity.getBatism().getLeaderName(),
+                    entity.getBatism().getBaptismDate()
+            );
+        };
+
+        if (entity.getLocation() != null) {
+           var location = new LocationDTO(
+                    entity.getLocation().getZipCode(),
+                    entity.getLocation().getStreet(),
+                    entity.getLocation().getHouseNumber(),
+                    entity.getLocation().getCity(),
+                    entity.getLocation().getState(),
+                    entity.getLocation().getNeighborhood()
+            );
+        }
         return entity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Adult adult = (Adult) o;
+        return Objects.equals(cpf, adult.cpf) && Objects.equals(location, adult.location) && Objects.equals(batism, adult.batism) && civilStatus == adult.civilStatus && Objects.equals(spouse, adult.spouse) && Objects.equals(children, adult.children) && role == adult.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), cpf, location, batism, civilStatus, spouse, children, role);
+    }
+
+    @Override
+    public String toString() {
+        return "Adult{" +
+                "cpf='" + cpf + '\'' +
+                ", location=" + location +
+                ", batism=" + batism +
+                ", civilStatus=" + civilStatus +
+                ", spouse=" + spouse +
+                ", children=" + children +
+                ", role=" + role +
+                '}';
     }
 }
 
