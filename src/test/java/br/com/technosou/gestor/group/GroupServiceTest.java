7c5e436fc4c10a4da13aaac1be9b8e635254a9db
Ramon <ramonrodsouza@gmail.com>
2025-09-09 11:43:42 -0300

    feature-49 - feat: Colocando Api Swaggger

---
+++ src/test/java/br/com/technosou/gestor/group/GroupServiceTest.java
@@ -0,0 +1,31 @@
+package br.com.technosou.gestor.group;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GroupServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ src/test/java/br/com/technosou/gestor/member/child/ChildServiceTest.java
@@ -0,0 +1,31 @@
+package br.com.technosou.gestor.member.child;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ChildServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
