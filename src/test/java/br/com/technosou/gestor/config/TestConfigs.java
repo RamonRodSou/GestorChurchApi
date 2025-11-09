9c820794ec2760c7e8a493df9e8026e199091534
Ramon <ramonrodsouza@gmail.com>
2025-11-08 21:07:24 -0300

    feature-62 - feat: Criação testes

---
+++ src/test/java/br/com/technosou/gestor/config/TestConfigs.java
@@ -0,0 +1,13 @@
+package br.com.technosou.gestor.config;
+
+public interface TestConfigs {
+
+    int SERVER_PORT = 8080;
+
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN= "Origin";
+    String ORIGIN_TECHNOSOU_TRUE = "http://www.technosou.com";
+    String ORIGIN_TECHNOSOU_FALSE = "http://www.technosou.com.br";
+
+
+}
