ac1d1c33c36935a839d96505c38d88a50e1a786a
Ramon <ramonrodsouza@gmail.com>
2025-11-08 21:06:43 -0300

    feature-60 - feat: Criação Adult Controller e DTO

---
+++ src/test/java/br/com/technosou/gestor/integrationtests/controllers/withjson/AdultControllerTest.java
@@ -0,0 +1,31 @@
+package br.com.technosou.gestor.integrationtests.controllers.withjson;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AdultControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
